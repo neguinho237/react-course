d147fbf889edddfd3388052608c241b6e965de2c
neguinho237 <brunomirands237@gmail.com>
2024-05-21 22:05:19 -0300

    feat:created main, content, section

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,18 @@
 import "./App.css"
 
-import Layout from "./components/Layout";
-import SuporteLogo from "./assets/images/svg/suporte-logo.jpeg"
-
-function App() {
+import Content from "./components/Team/Components/content.jsx";
+import Section from "./components/Team/Components/section.jsx"
+import Main from "./components/Team/Components/main.jsx";
+export default function App() {
   return (
     <body>
-
-      <Layout 
-        tag="react" 
-        title="Curso React - Senai" 
-        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique. Duis cursus, mi quis viverra ornare, eros dolor interdum nulla, ut commodo diam libero vitae erat." 
-        buttonLeftTitle="Leia mais" 
-        buttonRightTitle="Documentação" 
-        image={SuporteLogo} 
-        imageAlt="logo da suporte de domingo"
-      />
-      
+       <Section/>
+      <Content/>
+      <Main/>
+     
     </body>
   );
 }
 
-export default App;
 
 
---
+++ src/components/Team/Components/section.jsx
@@ -0,0 +1,10 @@
+import '../styles.css'
+export default function Section(){
+    return(
+        <div className='section-container'>
+            <h3>Tagline</h3>
+            <h1>Our team</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. </p>
+        </div>
+    )
+}
