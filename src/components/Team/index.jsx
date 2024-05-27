b43b3c5941ede0a84efd70aaadf70a35561092c4
neguinho237 <brunomirands237@gmail.com>
2024-05-27 17:39:43 -0300

    feat: team-composition complete

--- src/components/Team/index.jsx
+++ src/components/Team/index.jsx
@@ -26,10 +26,10 @@ const TeamUsers = [
     }
  },
     {
-    image: "https://i.pinimg.com/originals/37/fb/89/37fb896208d39fed48e4946a87f6444a.jpg",
-    name : "Assassin's Creed Odyssey",
+    image: "https://store-images.s-microsoft.com/image/apps.8098.14585440003614248.9f7109bf-73f7-4bc7-ba61-1eeb006d905a.222e4dfa-df01-4864-a10a-bc3666f1a3cf?mode=scale&q=90&h=1080&w=1920",
+    name : "Assassin's Creed Valhalla",
     role: "Jogo",
-    description: "Escreva sua própria odisséia épica",
+    description: "Crie sua própria lenda viking",
     mediaSocial: {
         linkedin: "https://br.linkedin.com/in/lucascorreaa/",
             x: "https://x.com/",
@@ -37,10 +37,10 @@ const TeamUsers = [
     }
  },
     {
-    image: "https://i.pinimg.com/originals/37/fb/89/37fb896208d39fed48e4946a87f6444a.jpg",
-    name : "Assassin's Creed Odyssey",
+    image: "https://m.media-amazon.com/images/M/MV5BYmEyYjRlYWYtYmMyNS00YTAzLWI2MTAtMDllNjU1NmVkOWVmXkEyXkFqcGdeQXVyMTUzMTUxMzk1._V1_FMjpg_UX1000_.jpg",
+    name : "Assassin's Creed Mirage",
     role: "Jogo",
-    description: "Escreva sua própria odisséia épica",
+    description: "Torne-se o assassino mais versatil da história da franquia",
     mediaSocial: {
         linkedin: "https://br.linkedin.com/in/lucascorreaa/",
             x: "https://x.com/",
@@ -48,10 +48,10 @@ const TeamUsers = [
     }
  },
     {
-    image: "https://i.pinimg.com/originals/37/fb/89/37fb896208d39fed48e4946a87f6444a.jpg",
-    name : "Assassin's Creed Odyssey",
+    image: "https://th.bing.com/th?id=OIF.fLr98qx7JJ%2fw%2fr6c9fVtcQ&rs=1&pid=ImgDetMain",
+    name : "Assassin's Creed Shadows",
     role: "Jogo",
-    description: "Escreva sua própria odisséia épica",
+    description: "Um novo credo surge",
     mediaSocial: {
         linkedin: "https://br.linkedin.com/in/lucascorreaa/",
             x: "https://x.com/",
@@ -59,21 +59,10 @@ const TeamUsers = [
     }
  },
     {
-    image: "https://i.pinimg.com/originals/37/fb/89/37fb896208d39fed48e4946a87f6444a.jpg",
-    name : "Assassin's Creed Odyssey",
+    image: "https://image.api.playstation.com/cdn/UP0001/CUSA05855_00/yYiGRi9oya2bnKV9QnZ65ghdgBTVWQfI.png",
+    name : "Assassin's Creed Origins",
     role: "Jogo",
-    description: "Escreva sua própria odisséia épica",
-    mediaSocial: {
-        linkedin: "https://br.linkedin.com/in/lucascorreaa/",
-            x: "https://x.com/",
-            dribble: "https://dribbble.com/" 
-    }
- },
-    {
-    image: "https://i.pinimg.com/originals/37/fb/89/37fb896208d39fed48e4946a87f6444a.jpg",
-    name : "Assassin's Creed Odyssey",
-    role: "Jogo",
-    description: "Escreva sua própria odisséia épica",
+    description: "Ande e descubra novas aventuras no Egito antigo",
     mediaSocial: {
         linkedin: "https://br.linkedin.com/in/lucascorreaa/",
             x: "https://x.com/",
