6f0838266406f1ee05c5d320e87557537b5ce3d0
neguinho237 <brunomirands237@gmail.com>
2024-06-17 16:48:31 -0300

    feat: style-modules

--- src/App.jsx
+++ src/App.jsx
@@ -8,19 +8,22 @@ import PageThree from "./pages/PageThree";
 import Navbar from "./hooks/components/Navbar"
 import GetCep from "./request";
 import GetPokemon from "./api/api-pokemon";
+import SingUp from "./styles/css-modules/components/singup"
 
 
 
 function App() {
   return (
     <body>
-      <Routes>
-        <Route path="/" Component={GetPokemon} />
+      {/* <Routes>
+        <Route path="/" Component={SingUp} />
         <Route path="/navbar" Component={Navbar} />
         <Route path="/page-one" Component={PageOne} />
         <Route path="/page-two" Component={PageTwo} />
         <Route path="/page-three" Component={PageThree} />
-      </Routes>
+      </Routes> */}
+
+      <SingUp/>
     </body>
   );
 }
--- src/styles/css-modules/components/singup/index.jsx
+++ src/styles/css-modules/components/singup/index.jsx
@@ -3,7 +3,9 @@ import Google from "../../../../assets/images/svg/Google.svg"
 
 export default function SingUp (){
     return(
+        
         <div className={styles.container}>
+            
             <div className={styles.header}>
                 <h1 className={styles.title}>Logo</h1>
             </div>
@@ -15,27 +17,29 @@ export default function SingUp (){
                 <p>Lorem ipsum dolor sit amet adipiscing elit.</p>
                 </div>
                 
-                <div className={styles.inputs}>
+                <div className={styles.AllInputs}>
                         
                         <div className={styles.inputOne}>
-                        <label htmlFor="name">Name</label>
+                        <label htmlFor="name">Name*</label>
                         <input type="text" id="name" Name = "name" />
                         </div>
                        
-                        <div className= {styles.inputTwo}>
-                        <label htmlFor="email">Email</label>
+                        <div className={styles.inputTwo}>
+                        <label htmlFor="email">Email*</label>
                         <input type="text" id="email" Email = "email" />
                         </div>
                        
                         <div className={styles.inputThree}>
-                        <label for="pwd">Password:</label>
+                        <label for="pwd">Password*</label>
                         <input type="password" id="pwd" name="pwd"></input>
                         </div>
                 </div>
+                
                 <div className={styles.buttons}>
-                    <button>Sign Up</button>
-                    <button> <img src= {Google} alt="google incon" /> Sign Up with Google</button>
+                    <button className={styles.buttonBlack}>Sign Up</button>
+                    <button className={styles.buttonGoogle}> <img src= {Google} alt="google incon" /> Sign Up with Google</button>
                 </div>
+                
                 <div className={styles.logIn}>
                     <p>Already have an account? <a href="LogIn">Log In</a> </p>
                 </div>
