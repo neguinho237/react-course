121138f0c6a9291df0af3ccb265885f83700108a
neguinho237 <brunomirands237@gmail.com>
2024-07-04 16:25:31 -0300

    finished

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -6,24 +6,45 @@ import Youtube from "./src/assets/images/Youtube.svg"
 
 export default function Footer (){
     return(
-        <main className="flex flex-col justify-center items-center ">
-            <section className="flex gap-40" >
-                <h1>Logo</h1>
-                <div className="flex gap-4">
-                <a href="link one">Link one</a>
-                <a href="link two">Link two</a>
-                <a href="link three">Link three</a>
-                <a href="link four">Link four</a>
-                <a href="link five">Link five</a>
+        <main className="flex flex-col justify-center items-center mt-20 ">
+            <section className="flex justify-center items-center gap-40" >
+                <h1 className="text-2xl font-semibold">Logo</h1>
+                <div className="flex justify-center items-center gap-4">
+                <a className="font-semibold" href="link one">Link one</a>
+                <a className="font-semibold" href="link one">Link one</a>
+                <a className="font-semibold" href="link two">Link two</a>
+                <a className="font-semibold" href="link three">Link three</a>
+                <a className="font-semibold" href="link four">Link four</a>
+                <a className="font-semibold" href="link five">Link five</a>
                 </div>
                 <div className="flex gap-5">
+                    <a target="blank" href="https://www.facebook.com/">
                     <img src={Facebook} alt="" />
+                    </a>
+                    <a target="blank" href="https://www.instagram.com/">
                     <img src={Instagram} alt="" />
+                    </a>
+                    <a target="blank" href="https://x.com/X">
                     <img src={X} alt="" />
+                    </a>
+                    <a target="blank" href="https://www.linkedin.com/home">
                     <img src={Linkedin} alt="" />
+                    </a>
+                    <a target="blank" href="https://www.youtube.com/">
                     <img src={Youtube} alt="" />
+                    </a>
                 </div>
             </section>
+            <div className="h-1 w-full border-solid border-black mt-20 " ></div>
+            <div className="flex gap-4 mt-10">
+                <h1>2023 Relume. All right reserved.</h1>
+                <div className="flex gap-6">
+                <a clas href="">Terms of Service</a>
+                <a clas href="">Cookies Settings</a>
+                <a clas href="">Privacy Policy</a>
+                </div>
+            </div>
         </main>
+        
     )
 }
